a2a89c371daa7a705854d8bef99ed61871826ad7
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2023-02-12 22:19:04 +0200

    Delete restapi directory

--- restapi/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$serverHost = "ls-ccb15469205b1aa769fdee7d225a9934157b66e3.ctavff5ziwdp.eu-central-1.rds.amazonaws.com";
-$user = "root";
-$password = "Ah778sbf8Cjx";
-$database = "newschema";
-
-$connectNow = new mysqli($serverHost, $user, $password, $database);
